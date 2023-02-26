6b996b54f44119c4ea07bbf7cb4245a64ad6dd51
Brismun <96andrei.dumirescu@gmail.com>
2023-02-26 10:33:57 +0100

    config done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        headF: ["Ubuntu", "Serif"],
+        paraF: ["Open Sans", "Serif"],
+      },
+      screens: {
+        gg: "700px",
+      },
+    },
   },
   plugins: [],
 };
