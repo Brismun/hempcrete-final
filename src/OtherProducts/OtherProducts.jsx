e7172b52aaa93cc1b620d38a5e2b14a6dac4b630
alex <alex_bakaj@yahoo.com>
2023-02-26 12:21:55 +0200

    2

---
+++ src/OtherProducts/OtherProducts.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+const OtherProducts = () => {
+  return (
+    <div className=" flex flex-col justify-center gap-7 bg-[#121212] ">
+      <div className=" flex  items-center gap-[50px] ">
+        <div className="h-[120px] border-l-8 border-[#7B8753]"></div>
+        <div className="flex flex-col gap-5 ">
+          <div className="flex items-center gap-[5px]">
+            <div className="w-[30px] border-t-2"></div>
+            <h2 className="text-white ">MORE OF OUR CONCEPT</h2>
+          </div>
+
+          <h1 className="text-xl text-[41px] font-bold text-white">
+            Other products
+          </h1>
+        </div>
+      </div>
+      <div className="mx-auto flex items-center gap-10   ">
+        <div>
+          <img
+            className="h-[275px] w-[275px] "
+            src="https://scontent.fotp7-2.fna.fbcdn.net/v/t39.30808-6/308126257_2119162038271096_584836703695818679_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFbT0q-8EGQpDtbT1VUZNQT9nQFdg4byIr2dAV2DhvIiqi056o81ZWwWZY6LOOY6A9hH66iz9kv_d61AoruZJBk&_nc_ohc=vhxrFFrJsSwAX85bjKq&_nc_ht=scontent.fotp7-2.fna&oh=00_AfB-hP0UbOzIn4uMyPfHaSAagks-Z9E8bqCHBBf0sybEPg&oe=63FC3F1D"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col flex-wrap justify-between gap-[30px] ">
+          <h1 className="font-headF text-white">Product 1</h1>
+          <p className=" mx-auto w-[753px] font-paraF  text-white">
+            {" "}
+            The importance of hempcrete binder can never be underestimated and
+            our products were carefully selected and tested to create long
+            lasting biocomposites that are resilient, healthy, and high
+            performing.
+            <br />
+            <br /> Hempitecture uses a specifically formulated mineral based
+            binder that is ideal for bonding hemp core both in spray applied and
+            cast in place methods.
+          </p>
+          <div className="mt-5">
+            <button className="btn-primary">Contact Us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OtherProducts;
