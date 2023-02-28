1b83601dc451d2e1f84b3ba89fbe1e4a3af53a0d
Andrei Dumitrescu <96andrei.dumitrescu@gmail.com>
2023-02-28 18:24:16 +0100

    Hero-Final

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -9,26 +9,25 @@ import Svg4 from "../Hero/SGVs/svgS";
 const div = () => {
   return (
     <div className="py-5">
-      <div className="flex flex-col items-center justify-center gap-8 gg:flex-row">
-        <div className="max-w[500px] text-center gg:text-left ">
-          <h1 className="font-headF text-[25px] text-[#D8B586] gg:text-[55px]">
+      <div className="flex flex-col items-center justify-center gg:flex-row gg:gap-[114px]">
+        <div className="max-w-[500px] text-center gg:text-left ">
+          <h1 className="font-headF text-[25px] text-[#D8B586] gg:mb-[15px] gg:text-[55px]">
             HEMPCRETE
           </h1>
-          <h1 className="whitespace-nowrap pb-2 font-headF text-[25px] leading-none text-[#D8B586] gg:p-0 gg:text-[55px]">
+          <h1 className="whitespace-nowrap font-headF text-[25px] leading-none text-[#D8B586] gg:mb-[15px] gg:text-[55px]">
             BUILDING SYSTEMS
           </h1>
 
-          <p className="pt-2 pb-4 font-paraF text-[16px] text-white">
+          <p className="max-w[500px] my-[1.8rem] font-paraF text-[16px] text-white gg:mb-[25px]">
             Hempitecture is the US leader in hemp construction. We work with
-            <br></br> architects, builders, engineers, and developers to build
-            energy efficient,<br></br> non-toxic, and carbon absorbing
-            environments. We can help you use hemp<br></br> based insulation
-            materials on your next project.
+            architects, builders, engineers, and developers to build energy
+            efficient, non-toxic, and carbon absorbing environments. We can help
+            you use hemp based insulation materials on your next project.
           </p>
           <div className="max-w-[548px] bg-[#D8B586] py-1 text-center align-middle font-headF text-[20px] font-bold text-black">
             <h2 className="">BENEFITS OF HEMPCRETE</h2>
           </div>
-          <div className="paddingY flex flex-col items-center gg:flex-row gg:gap-[5rem]">
+          <div className="paddingY flex flex-col items-center gap-3 gg:flex-row gg:gap-[5rem]">
             <div className="flex flex-col">
               <div className="flex flex-col items-center gap-1 text-center gg:flex-row gg:text-left">
                 <Svg1 />
